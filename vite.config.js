922c39128110774a8795e01f58322e38f3ca0dcd
Wanus <wanus@Wanusui-MacBook-Pro-2.local>
2026-03-15 17:08:58 +0900

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/wando-workation/',  // ← GitHub 레포 이름으로 변경
+  base: '/wando_workation/',  // ← GitHub 레포 이름으로 변경
 })
